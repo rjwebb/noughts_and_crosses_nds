62f55c543a89076020bf0eb93980538aa96ad650
Bob Webb <bob.wbb@gmail.com>
2024-12-27 21:12:18 +0000

    display sprites, support multiple images with a shared palette, use 32x32 sprites for now

--- project/source/main.c
+++ project/source/main.c
@@ -1,13 +1,13 @@
-#include <crab.h>
+#include <combined32.h>
 #include <nds.h>
 #include <stdio.h>
 #include <string.h>
 
-// glImage Crab[1];
+enum CellState { S_EMPTY = 0, S_GOOSE = 1, S_CRAB = 2 };
 
 int main(void) {
   // put the main screen on the bottom lcd
-  // lcdMainOnBottom();
+  lcdMainOnBottom();
 
   // Sprite initialisation
   videoSetMode(MODE_0_2D);
@@ -16,36 +16,71 @@ int main(void) {
 
   int keys = 0;
 
-  oamInit(&oamMain, SpriteMapping_1D_64, false);
+  int spriteTilesLen = 32 * 32;
+
+  oamInit(&oamMain, SpriteMapping_1D_128, false);
   vramSetBankA(VRAM_A_MAIN_SPRITE);
 
-  u16 *gfx =
-      oamAllocateGfx(&oamMain, SpriteSize_64x64, SpriteColorFormat_256Color);
+  // copy in the palette - this is shared by all of the sprites
+  dmaCopy(combined32Pal, SPRITE_PALETTE, combined32PalLen);
 
-  // copy in our ball graphics
-  dmaCopy(crabBitmap, gfx, crabBitmapLen);
-  dmaCopy(crabPal, SPRITE_PALETTE, crabPalLen);
+  // empty sprite
+  u16 *emptyGfx =
+      oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
+  u8 *emptyTiles = (u8 *)combined32Tiles;
+  dmaCopy(emptyTiles, emptyGfx, spriteTilesLen);
+
+  // goose sprite
+  u16 *gooseGfx =
+      oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
+  u8 *gooseTiles = (u8 *)combined32Tiles + spriteTilesLen;
+  dmaCopy(gooseTiles, gooseGfx, spriteTilesLen);
+
+  // crab sprite
+  u16 *crabGfx =
+      oamAllocateGfx(&oamMain, SpriteSize_32x32, SpriteColorFormat_256Color);
+  u8 *crabTiles = (u8 *)combined32Tiles + (spriteTilesLen * 2);
+  dmaCopy(crabTiles, crabGfx, spriteTilesLen);
+
+  int cells[3][3] = {{S_EMPTY, S_EMPTY, S_EMPTY},
+                     {S_EMPTY, S_EMPTY, S_EMPTY},
+                     {S_EMPTY, S_EMPTY, S_EMPTY}};
+
+  int gridXStart = 80;
+  int gridYStart = 48;
 
   while (pmMainLoop()) {
 
     scanKeys();
     keys = keysHeld();
 
-    oamSet(&oamMain,         // sub display
-           0,                // oam entry to set
-           0, 0,             // position
-           0,                // priority
-           0,                // palette for 16 color sprite or alpha for
-                             // bmp sprite
-           SpriteSize_64x64, // sprite size
-           SpriteColorFormat_256Color, // sprite color fornat
-           gfx,                        // pointer to the loaded graphics
-           0,                          // affine rotation to use
-           false,                      // double the size of rotated sprites
-           false,                      // don't hide the sprite
-           false, false,               // vflip, hflip
-           false                       // apply mosaic
-    );
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        int cell = cells[i][j];
+        u16 *gfx = emptyGfx;
+        if (cell == S_GOOSE) {
+          gfx = gooseGfx;
+        } else if (cell == S_CRAB) {
+          gfx = crabGfx;
+        }
+
+        oamSet(&oamMain,                                 // sub display
+               i * 3 + j,                                // oam entry to set
+               gridXStart + j * 32, gridYStart + i * 32, // position
+               0,                                        // priority
+               0,                // palette for 16 color sprite or alpha for
+                                 // bmp sprite
+               SpriteSize_32x32, // sprite size
+               SpriteColorFormat_256Color, // sprite color fornat
+               gfx,                        // pointer to the loaded graphics
+               0,                          // affine rotation to use
+               false,                      // double the size of rotated sprites
+               false,                      // don't hide the sprite
+               false, false,               // vflip, hflip
+               false                       // apply mosaic
+        );
+      }
+    }
 
     swiWaitForVBlank();
 
